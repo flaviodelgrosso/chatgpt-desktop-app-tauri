d37897d2415da530b4455df6786143462b581184
Flavio Del Grosso <flaviodelgrosso@live.it>
2023-01-09 23:41:13 +0100

    release script

---
+++ scripts/release.js
@@ -0,0 +1,48 @@
+import { execSync } from "child_process";
+import fs from "fs";
+import k from "kleur";
+
+import { $argv, tauriConfJSON, relativePath, TAURI_CONF_PATH, PACKAGE_PATH, packageJSON } from "./utils.js";
+
+(async () => {
+  /** Args can be: --git --minor --major */
+  const argv = $argv();
+  const tauriConfJson = tauriConfJSON();
+  const pkg = packageJSON();
+  const version = tauriConfJson?.package?.version || "0.0.0";
+  let [a, b, c] = version.split(".")?.map(Number);
+
+  if (argv.major) {
+    a += 1;
+    b = 0;
+    c = 0;
+  } else if (argv.minor) {
+    b += 1;
+    c = 0;
+  } else {
+    c += 1;
+  }
+
+  const nextVersion = `${a}.${b}.${c}`;
+  const oldVersion = version;
+  tauriConfJson.package.version = nextVersion;
+  pkg.version = nextVersion;
+
+  // rewrite tauri.conf.json
+  fs.writeFileSync(TAURI_CONF_PATH, JSON.stringify(tauriConfJson, null, 2));
+  // rewrite package.json
+  fs.writeFileSync(PACKAGE_PATH, JSON.stringify(pkg, null, 2));
+  const verChange = `(${k.yellow(oldVersion)} -> ${k.green(nextVersion)})`;
+
+  if (argv.git) {
+    execSync(`git add ${relativePath(TAURI_CONF_PATH)} ${relativePath(PACKAGE_PATH)}`);
+    execSync(`git commit -m "v${nextVersion}"`);
+    execSync(`git tag -a v${nextVersion} -m "v${nextVersion}"`);
+    execSync(`git push`);
+    execSync(`git push origin v${nextVersion}`);
+    console.log(k.green("[✨ release]"), `Published successfully ${verChange}`);
+  } else {
+    console.log(k.green("[✨ release]"), `tauri.conf.json version ${verChange}`);
+    console.log(k.green("[✨ release]"), `package.json version ${verChange}`);
+  }
+})();
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,9 +1,15 @@
 import path from "path";
 import minimist from "minimist";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+
+export const ROOT_PATH = process.cwd();
+export const PACKAGE_PATH = path.join(ROOT_PATH, "package.json");
+export const TAURI_CONF_PATH = path.join(ROOT_PATH, "src-tauri", "tauri.conf.json");
+export const UPDATER_JSON_PATH = path.join(ROOT_PATH, "updater", "releases.json");
 
 export const $argv = () => minimist(process.argv.slice(2));
 export const relativePath = (p) => `${p.split(ROOT_PATH)?.[1].substring(1)}` || "";
-
-export const ROOT_PATH = process.cwd();
-export const TAURI_CONF_PATH = path.join(ROOT_PATH, "src-tauri", "tauri.conf.json");
-export const UPDATER_JSON_PATH = path.join(ROOT_PATH, "updater", "releases.json");
+export const tauriConfJSON = () => require(TAURI_CONF_PATH);
+export const packageJSON = () => require(PACKAGE_PATH);
